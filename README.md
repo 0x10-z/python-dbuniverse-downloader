e9b71e9f26c43e5062eefa65d7d1583902349a1e
Iker <ikerocio@yahoo.es>
2014-10-26 17:58:35 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-.
+# python-dbuniverse-downloader
+
+## Overview
+
+PDF downloader about [http://dragonball-multiverse.com](http://dragonball-multiverse.com)'s Drago Ball comic.
+
+## Installation
+
+Clone the project from github
+
+    $ git clone git@github.com:iocio005/python-dbuniverse-downloader.git
+    $ cd python-dbuniverse-downloader
+
+## How to use it?
+If the last chapter in [Dragon Ball Multiverse comic](http://www.dragonball-multiverse.com/es/chapters.html) is 949...
+
+    $ python dbmultiverse.py 848
+
+Wait until the program is finished and enjoy the comic!
+
+## Changelog
+## 0.1
+
+**26th Oct 2014**
+
+* First release
+
---
+++ dbmultiverse.py
@@ -0,0 +1,147 @@
+#! /usr/bin/python
+# -*- coding: utf8 -*-
+import requests, time, lxml.html, os
+from urlparse import urljoin
+from PyPDF2 import PdfFileMerger, PdfFileReader
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import cm
+from PIL import Image
+import sys
+
+def get_url_from_pne(path, fname, number, fext):
+    return os.path.join(path, fname+str(number)+'.'+fext)
+
+BLACK_LIST = []
+EXTRA_IMAGES = []
+ROOT_DIRECTORY = os.path.dirname(__file__)
+# Las 3 variables de acontinuacion se deben modificar para cada usuario. Colocar la ruta de donde se va a descargar cada cosa
+pdf_path = os.path.join(ROOT_DIRECTORY, 'pdf') # La ruta donde se van a guardar los PDF
+img_temp = os.path.join(ROOT_DIRECTORY, 'images') # La ruta donde se guardaran las imagenes
+
+parent_url = 'http://www.dragonball-multiverse.com/es/'
+
+""" download all images from html """
+def download():
+    i = 0
+    aux = 0
+    for i in range(int(MAX_BOOKS)):
+    	try:
+            if not os.path.isfile(get_url_from_pne(img_temp, 'DBM_', i, 'jpg')) and not os.path.isfile(get_url_from_pne(img_temp, 'DBM_', i, 'png')):
+                link = 'http://www.dragonball-multiverse.com/es/page-'+str(i)+'.html'
+                source = requests.get(link).content
+                html = lxml.html.fromstring(source)
+                book_title = html.cssselect('div')[7].cssselect('img')[0].get('src')
+                url=urljoin(parent_url,book_title)
+                img_format = book_title[-3:] #cogemos la extension del fichero para que no importe si es ".png" o ".jpg"
+                img_file = open(get_url_from_pne(img_temp, 'DBM_', i, img_format),'wb') #abrimos el fichero donde guardaremos la imagen
+                #img_file.write(urllib.urlopen(url).read()) #empezamos la descarga
+                img_file.write(requests.get(url).content)
+                img_file.close() #cerramos el fichero
+                time.sleep(0.5)
+                print "Downloaded in: "+get_url_from_pne(img_temp, 'DBM_', i, img_format)
+            else:
+                print "Saltando libro numero %s\r" %i
+
+        except Exception as e:
+            print e
+            print "No se ha podido descargar"
+            BLACK_LIST.append(i)
+            time.sleep(.5)
+
+
+""" merge all pdfs in one """
+def merge_pdf():
+    merger = PdfFileMerger()
+    num = 0
+    for num in range(MAX_BOOKS):
+        if num not in BLACK_LIST:
+            merger.append(PdfFileReader(file(get_url_from_pne(pdf_path, 'DBM_', num, 'pdf'), 'rb')))
+            print 'uniendo pagina %i...'%num
+        else:
+            print 'El numero %i esta en la BLACK LIST'%num
+    merger.write(os.path.join(ROOT_DIRECTORY,'DragonBallMultiverse.pdf'))
+    print 'Terminado de unir'
+
+
+""" this method copy all images in a PDF"""
+def img_to_pdf():
+    aux_X = 0
+    aux_Y = 0
+    num = 0
+    print 'Checkenado ancho de imagenes...'
+    for num in range(MAX_BOOKS):
+        if (os.path.isfile(get_url_from_pne(img_temp, 'DBM_', num, 'jpg'))): #Tratamos diferente la imagen si es .png o .jpg
+            check_images_width(get_url_from_pne(img_temp, 'DBM_', num, 'jpg'))
+        elif(os.path.isfile(get_url_from_pne(img_temp, 'DBM_', num, 'png'))):
+            check_images_width(get_url_from_pne(img_temp, 'DBM_', num, 'png'))
+    for num in range(MAX_BOOKS+len(EXTRA_IMAGES)):
+        try:
+            if num not in BLACK_LIST:
+                c = canvas.Canvas(get_url_from_pne(pdf_path, 'DBM_', num, 'pdf'))
+                if (os.path.isfile(get_url_from_pne(img_temp, 'DBM_', num, 'jpg'))): #Tratamos diferente la imagen si es .png o .jpg
+                    if get_url_from_pne(img_temp, 'DBM_', num, 'jpg') in EXTRA_IMAGES:
+                        for aux in range(1, 3): #captura las imagenes cortadas muy anchas
+                            c.drawImage(get_url_from_pne(img_temp, 'DBM_'+str(num)+'_',aux,'jpg'), aux_X, aux_Y, 21*cm, 28*cm)
+                            c.showPage()
+                            c.save()
+                    else:
+                        c.drawImage(get_url_from_pne(img_temp, 'DBM_', num, 'jpg'), aux_X, aux_Y, 21*cm, 28*cm) # abrimos imagen y dibujamos en pdf. 21 y 28 son las medidas para las imagenes
+                        c.showPage()
+                        c.save()
+                elif(os.path.isfile(get_url_from_pne(img_temp, 'DBM_', num, 'png'))):
+                    if get_url_from_pne(img_temp, 'DBM_', num, 'png') in EXTRA_IMAGES:
+                        for aux in range(1, 3):
+                            c.drawImage(get_url_from_pne(img_temp, 'DBM_'+str(num)+'_',aux,'png'), aux_X, aux_Y, 21*cm, 28*cm)
+                            c.showPage()
+                            c.save()
+                    else:
+                        c.drawImage(get_url_from_pne(img_temp, 'DBM_', num, 'png'), aux_X, aux_Y, 21*cm, 28*cm) #lo mismo que arriba pero para .png
+                        c.showPage()
+                        c.save()
+
+                print "libro numero "+str(num)
+            else:
+                print 'El numero %i esta en la BLACK LIST'%num
+        except Exception as e:
+            print e
+            print "numero no encontrado"
+
+
+
+def prepare_dirs(IMG_DIR, PDF_DIR):
+    if not os.path.exists(IMG_DIR):
+        os.makedirs(IMG_DIR)
+    if not os.path.exists(PDF_DIR):
+        os.makedirs(PDF_DIR)
+
+def check_images_width(url = 'images/DBM_8.jpg'):
+    img = Image.open(url)
+    fname, fext = url.split("/")[-1].split(".")[0], url.split("/")[-1].split(".")[1]
+    width, height = img.size[0], img.size[1]
+    if width > 1000:
+        img_2, img_1 = img.crop((width/2, 0, width, height)), img.crop((0, 0, width/2, height)) #left - top - right - bottom
+        img_1.save(os.path.join(img_temp, fname+'_1.'+fext))
+        img_2.save(os.path.join(img_temp, fname+'_2.'+fext))
+        EXTRA_IMAGES.append(url)
+    else:
+        pass
+
+def all():
+    download()
+    img_to_pdf()
+    merge_pdf()
+    print 'FINISH'
+
+""" interactive user mode """
+print "============================================="
+print "---------Script realizado por BashRC---------"
+print "--------Para mis amigos de forocarros--------"
+print "---------------------------------------------"
+print "=============================================\n"
+MAX_BOOKS = 0
+if __name__ == '__main__':
+    MAX_BOOKS = int(sys.argv[1])
+    try:
+        all()
+    except Exception as e:
+        print e
